ef5ab193d11c0ede0a96b2598deef1ff8c309379
octakitten <raspberry2425@gmail.com>
2025-05-24 01:23:28 -0400

    also copy to final build dir

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@
 cp bin/tensorplus.so src/tensorplus/tensorplus.so
 cp bin/tensorplus.dylib src/tensorplus/tensorplus.dylib
 cp bin/tensorplus.dll src/tensorplus/tensorplus.dll
+
+cp bin/tensorplus.so dist/tensorplus.so
+cp bin/tensorplus.dylib dist/tensorplus.dylib
+cp bin/tensorplus.dll dist/tensorplus.dll
