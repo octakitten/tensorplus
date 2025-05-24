28c32e72612b97889720f65ea70eefb1d51b0324
octakitten <raspberry2425@gmail.com>
2025-05-24 00:26:02 -0400

    adding build script to cicd pipeline

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-nix build --impure --extra-experimental-features nix-command \
-  --extra-experimental-features flakes
-cp result/bin/bin/tensorplus.so src/tensorplus/tensorplus.so
-cp result/bin/bin/tensorplus.dylib src/tensorplus/tensorplus.dylib
-cp result/bin/bin/tensorplus.dll src/tensorplus/tensorplus.dll
+cp bin/tensorplus.so src/tensorplus/tensorplus.so
+cp bin/tensorplus.dylib src/tensorplus/tensorplus.dylib
+cp bin/tensorplus.dll src/tensorplus/tensorplus.dll
