eebdd91365ecf14b95d362b61101e5f6c6ebff1d
octakitten <raspberry2425@gmail.com>
2025-09-14 16:13:37 -0400

    deprecating the use of nix build process

--- nixbuild.sh
+++ nixbuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#now deprecated, use build.sh
 nix build --impure --extra-experimental-features nix-command \
   --extra-experimental-features flakes
 cp result/bin/bin/tensorplus.so src/tensorplus/tensorplus.so
