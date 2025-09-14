d97113f66bdebed083422305b44f3b30eb5637ab
octakitten <raspberry2425@gmail.com>
2025-09-14 16:51:09 -0400

    only build for linux target

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ make clean
 
 make 
 
-cp bin/tensorplus.so src/tensorplus/tensorplus.so
-cp bin/tensorplus.dylib src/tensorplus/tensorplus.dylib
-cp bin/tensorplus.dll src/tensorplus/tensorplus.dll
+cp tensorplus.so src/tensorplus/tensorplus.so
+#cp bin/tensorplus.dylib src/tensorplus/tensorplus.dylib
+#cp bin/tensorplus.dll src/tensorplus/tensorplus.dll
 
 #rm dist/tensorplus.so
 #rm dist/tensorplus.dylib
 #rm dist/tensorplus.dll
 
-cp bin/tensorplus.so dist/tensorplus.so
-cp bin/tensorplus.dylib dist/tensorplus.dylib
-cp bin/tensorplus.dll dist/tensorplus.dll
+cp tensorplus.so dist/tensorplus.so
+#cp bin/tensorplus.dylib dist/tensorplus.dylib
+#cp bin/tensorplus.dll dist/tensorplus.dll
